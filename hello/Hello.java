47b2155383b188b97ca2ae587d03a82b935187eb
Darren Kessner <Darren.Kessner@marlborough.org>
2022-09-06 14:24:19 -0700

    added hello folder

---
+++ hello/Hello.java
@@ -0,0 +1,20 @@
+
+
+// this is a comment
+
+/*
+this is a multiline
+comment
+*/
+
+
+public class Hello
+{
+  public static void main(String[] args)
+  {
+    for (int i=0; i<5; i++)
+    {
+      System.out.println("Hello, world!"); // print something
+    }
+  }
+}
